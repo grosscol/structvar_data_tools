66710743d4728dd2d98958dda8ee5c173cc083a1
Colin Gross <grosscol@gmail.com>
2024-06-27 15:49:56 -0400

    Memory test and report additional binary

--- build_and_grind.sh
+++ build_and_grind.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Run from root directory.
 # Convenience method for watching changes with entr:
 #   fdfind '\.(cpp|hpp|in)$' src include test | entr -c ./build_and_grind.sh
@@ -5,55 +6,69 @@ set -e
 cmake --build ./build
 
 
-INPUT_DATA=het_hom_selector/test/data/structvar_sample_input.vcf
-BINARY=build/het_hom_selector/bin/het_hom_sel
+HHS_DATA=het_hom_selector/test/data/structvar_sample_input.vcf
+HHS_BIN=build/het_hom_selector/bin/het_hom_sel
+
+echo "Het Home Selector Valgrind"
 
 # Positional args for random sample of file input
 valgrind --show-leak-kinds=definite,indirect,possible --leak-check=full \
-  --verbose --track-origins=yes --log-file=/tmp/valgrind.rnd.out \
-  ${BINARY} rnd ${INPUT_DATA} > /tmp/hhs_rnd.out 2> /tmp/hhs_rnd.err
+  --verbose --track-origins=yes --log-file=/tmp/vg.hhs.rnd.out \
+  ${HHS_BIN} rnd ${HHS_DATA} > /tmp/hhs_rnd.out 2> /tmp/hhs_rnd.err
 
 # Positional args for all samples and variant ids of file input
 valgrind --show-leak-kinds=definite,indirect,possible --leak-check=full \
-  --verbose --track-origins=yes --log-file=/tmp/valgrind.all.out \
-  ${BINARY} --emit-id all ${INPUT_DATA} > /tmp/hhs_all.out 2> /tmp/hhs_all.err
+  --verbose --track-origins=yes --log-file=/tmp/vg.hhs.all.out \
+  ${HHS_BIN} --emit-id all ${HHS_DATA} > /tmp/hhs_all.out 2> /tmp/hhs_all.err
 
 # Input via stdin
 valgrind --show-leak-kinds=definite,indirect,possible --leak-check=full \
-  --verbose --track-origins=yes --log-file=/tmp/valgrind.rnd.stdin.out \
-  ${BINARY} rnd < ${INPUT_DATA} > /tmp/hhs_rnd.stdin.out 2> /tmp/hhs_rnd.stdin.err
+  --verbose --track-origins=yes --log-file=/tmp/vg.hhs.rnd.stdin.out \
+  ${HHS_BIN} rnd < ${HHS_DATA} > /tmp/hhs_rnd.stdin.out 2> /tmp/hhs_rnd.stdin.err
 
 # Positional args for bad input
 valgrind --show-leak-kinds=definite,indirect,possible --leak-check=full \
-  --verbose --track-origins=yes --log-file=/tmp/valgrind.nonzero.out \
-  ${BINARY} rnd test/data/no_such_file.vcf > /tmp/nonzero.out 2> /tmp/nonzero.err
+  --verbose --track-origins=yes --log-file=/tmp/vg.hhs.nonzero.out \
+  ${HHS_BIN} rnd test/data/no_such_file.vcf > /tmp/nonzero.out 2> /tmp/nonzero.err
 
-echo "Running Valgrind"
-
-echo "Rnd Samples Output:"
-tail /tmp/hhs_rnd.out
-echo ""
-echo "All Samples Output:"
-tail /tmp/hhs_all.out
 
 echo ""
 echo "------ VALGRIND: RND ----------------------"
 echo -n "|"
-grep -A4 'LEAK SUMMARY' < /tmp/valgrind.rnd.out | grep -o 'lost:.*bytes' | tr '\n' '|'
+grep -A4 'LEAK SUMMARY' < /tmp/vg.hhs.rnd.out | grep -o 'lost:.*bytes' | tr '\n' '|'
 echo ""
 
 echo "------ VALGRIND: ALL ----------------------"
 echo -n "|"
-grep -A4 'LEAK SUMMARY' < /tmp/valgrind.all.out | grep -o 'lost:.*bytes' | tr '\n' '|'
+grep -A4 'LEAK SUMMARY' < /tmp/vg.hhs.all.out | grep -o 'lost:.*bytes' | tr '\n' '|'
 echo ""
 
 echo "------ VALGRIND: RND STDIN ----------------"
 echo -n "|"
-grep -A4 'LEAK SUMMARY' < /tmp/valgrind.rnd.stdin.out | grep -o 'lost:.*bytes' | tr '\n' '|'
+grep -A4 'LEAK SUMMARY' < /tmp/vg.hhs.rnd.stdin.out | grep -o 'lost:.*bytes' | tr '\n' '|'
 echo ""
 
 echo "------ VALGRIND: EXIT 1 -------------------"
 echo -n "|"
-grep -A4 'LEAK SUMMARY' < /tmp/valgrind.nonzero.out | grep -o 'lost:.*bytes' | tr '\n' '|'
+grep -A4 'LEAK SUMMARY' < /tmp/vg.hhs.nonzero.out | grep -o 'lost:.*bytes' | tr '\n' '|'
+echo ""
+echo "-------------------------------------------"
+
+
+echo "Read Summarizer Valgrind"
+
+RS_DATA=cram_summarizer/test/data/del_1_sample_1.sam
+RS_BIN=build/cram_summarizer/bin/read_summ
+
+# Positional args for random sample of file input
+valgrind --show-leak-kinds=definite,indirect,possible --leak-check=full \
+  --verbose --track-origins=yes --log-file=/tmp/vg.rs.rnd.out \
+  ${RS_BIN} ${RS_DATA} > /tmp/rs_rnd.out 2> /tmp/rs_rnd.err
+
+echo ""
+echo "------ VALGRIND: SAM ----------------------"
+echo -n "|"
+grep -A4 'LEAK SUMMARY' < /tmp/vg.rs.rnd.out | grep -o 'lost:.*bytes' | tr '\n' '|'
+grep 'All heap blocks were freed' < /tmp/vg.rs.rnd.out | cut --complement -d ' ' -f 1
 echo ""
 echo "-------------------------------------------"
